b74813f09a0409b8c5348758477644bc713c96ee
soumaya harizi <soumayaharizi366@gmail.com>
2024-12-19 11:27:11 +0100

    Commit new version

--- src/components/admin/articles/Listarticles.jsx
+++ src/components/admin/articles/Listarticles.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Listarticles = () => {
+  const [articles,setArticles]=useState([])
+  const fetcharticles=async()=>{
+    try{
+        const res=await axios.get("https://backend-ecommerce-one-bay.vercel.app/api/")
+       console.log(res)
+
+    }catch{
+      console.log(error);
+
+    }
+  }
   return (
-    <div>Liste des articles</div>
+    <div>
+      <button onClick={()=>fetcharticles()}>Afficher</button>
+      <h1> Liste des articles</h1>
+    </div>
   )
 }
 
-export default Listarticles
+export default Listarticles;
